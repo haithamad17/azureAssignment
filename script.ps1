f5b1b5a7732382254f600ee93b7d185a16ba6c52
haithamad17 <63776827+haithamad17@users.noreply.github.com>
2021-06-11 14:40:55 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,18 +1,41 @@
-#the first line is to login to the account
-az login 
-#.\azcopy login --tenant-id "a744ad3c-a378-440b-95ad-97af5c23c30a"
-#the next two lines is to create a container for each storage account
-.\azcopy make "https://chelseastorage.blob.core.windows.net/sourcecontainer"
-.\azcopy make "https://chelseastorage2.blob.core.windows.net/targetcontainer"
+Connect-AzAccount
+
+Get-AzLocation | select Location
+$location = "eastus"
+
+#creating a resource group
+$rGroup = "0307Test"
+New-AzResourceGroup -Name $rGroup -Location $location
+
+#creating the first storage account named "chelseastorage" with blob encryption
+$storageAccount1 = New-AzStorageAccount -ResourceGroupName $rGroup `
+  -Name "chelseastorage" `
+  -SkuName Standard_LRS `
+  -Location $location `
+$ctx1 = $storageAccount1.Context
+#creating the second storage account named "chelseastorage2" with blob encryption
+$storageAccount2 = New-AzStorageAccount -ResourceGroupName $rGroup `
+  -Name "chelseastorage2" `
+  -SkuName Standard_LRS `
+  -Location $location `
+$ctx2 = $storageAccount2.Context
+
+#creating the first container for chelseastorage 
+$container1Name = "sourcecontainer"
+New-AzStorageContainer -Name $containerName -Context $ctx1 -Permission blob
+#creating the second storage for chelseastorage2
+$container2Name = "targetcontainer"
+New-AzStorageContainer -Name $containerName -Context $ctx2 -Permission blob
 For ($i=0; $i -le 100; $i++) {
-    
     $file = "C:\blob$i.txt"
-    #creating a .txt file in C:/
     New-Item $file -ItemType File -Value "The first sentence in our file."
     Add-Content $file "The second sentence in our file."
     #Set-Content blob$i.txt 'test text file'
-    #uploading the blob files to the first storage account
-    .\azcopy copy $file "https://chelseastorage.blob.core.windows.net/sourcecontainer/?sv=2020-02-10&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-06-10T15:52:22Z&st=2021-06-10T07:52:22Z&spr=https&sig=jSaKedsMy6jWFHY6rbvbeVVaxcP5MlS8K5lgci3lbMo%3D"
+    Set-AzStorageBlobContent -File "C:\blob$i.txt" `
+      -Container $container1Name `
+      -Blob "C:\blob$i.txt" `
+      -Context $ctx1 
 }
-#copying the blobs from first storage account to the second storage account
-.\azcopy copy 'https://chelseastorage.blob.core.windows.net/sourcecontainer/?sv=2020-02-10&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-06-10T15:52:22Z&st=2021-06-10T07:52:22Z&spr=https&sig=jSaKedsMy6jWFHY6rbvbeVVaxcP5MlS8K5lgci3lbMo%3D' 'https://chelseastorage2.blob.core.windows.net/targetcontainer' --recursive
+azcopy login
+#azcopy copy 'C:\myDirectory\myTextFile.txt' 'https://mystorageaccount.blob.core.windows.net/mycontainer/myTextFile.txt'
+azcopy copy 'https://chelseastorage.blob.core.windows.net/sourcecontainer/?sv=2020-02-10&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-06-10T15:52:22Z&st=2021-06-10T07:52:22Z&spr=https&sig=jSaKedsMy6jWFHY6rbvbeVVaxcP5MlS8K5lgci3lbMo%3D' 'https://chelseastorage2.blob.core.windows.net/targetcontainer' --recursive
